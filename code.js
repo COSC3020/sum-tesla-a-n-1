73dbe2feb376760c6f4f30f298e0ba55b45b13ef
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-01-23 15:41:43 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function sum(a) {
-    var sum = a[0];
+    var sum = 0; // change a[0] to 0 so there's no double counting and the elements are added starting from 0
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
